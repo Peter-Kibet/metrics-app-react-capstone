c1cb611f84fd9e69715e28af521b84191ed1a27d
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-26 14:25:57 +0300

    Fix linter errors

--- src/services/fetchCoin.js
+++ src/services/fetchCoin.js
@@ -1,35 +1,29 @@
-// services/fetchCoin.js
 import axios from 'axios';
 
 const API_URL = 'https://api.coingecko.com/api/v3/coins/';
 
 const fetchSingleCoin = async (coinId) => {
-  try {
-    const response = await axios.get(`${API_URL}${coinId}`);
-    const coin = response.data;
-    // create an object with the data you need
-    const coinData = {
-      id: coin.id,
-      name: coin.name,
-      price: coin.market_data.current_price.usd,
-      market_cap_rank: coin.market_cap_rank,
-      market_cap: coin.market_data.market_cap.usd,
-      high_24h: coin.market_data.high_24h.usd,
-      low_24h: coin.market_data.low_24h.usd,
-      price_change: coin.market_data.price_change_24h,
-      market_cap_change: coin.market_data.market_cap_change_24h,
-      total_supply: coin.market_data.total_supply,
-      max_supply: coin.market_data.max_supply,
-      circulating_supply: coin.market_data.circulating_supply,
-      image: coin.image.large,
-      description: coin.description.en,
-    };
-    // return the object
-    return coinData;
-  } catch (error) {
-    console.error('Error fetching single coin:', error);
-    throw error;
-  }
+  const response = await axios.get(`${API_URL}${coinId}`);
+  const coin = response.data;
+
+  const coinData = {
+    id: coin.id,
+    name: coin.name,
+    price: coin.market_data.current_price.usd,
+    market_cap_rank: coin.market_cap_rank,
+    market_cap: coin.market_data.market_cap.usd,
+    high_24h: coin.market_data.high_24h.usd,
+    low_24h: coin.market_data.low_24h.usd,
+    price_change: coin.market_data.price_change_24h,
+    market_cap_change: coin.market_data.market_cap_change_24h,
+    total_supply: coin.market_data.total_supply,
+    max_supply: coin.market_data.max_supply,
+    circulating_supply: coin.market_data.circulating_supply,
+    image: coin.image.large,
+    description: coin.description.en,
+  };
+
+  return coinData;
 };
 
 export default fetchSingleCoin;
--- src/services/fetchCoins.js
+++ src/services/fetchCoins.js
@@ -1,25 +1,19 @@
-// services/fetchCoins.js
 import axios from 'axios';
 
 const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false';
 
 const fetchCoins = async () => {
-  try {
-    const response = await axios.get(API_URL);
-    const coins = response.data;
-    // create an array of objects with the data you need
-    const coinsData = coins.map((coin) => ({
-      id: coin.id,
-      name: coin.name,
-      image: coin.image,
-      current_price: coin.current_price,
-    }));
-    // return the array
-    return coinsData;
-  } catch (error) {
-    console.error('Error fetching coins:', error);
-    throw error;
-  }
+  const response = await axios.get(API_URL);
+  const coins = response.data;
+
+  const coinsData = coins.map((coin) => ({
+    id: coin.id,
+    name: coin.name,
+    image: coin.image,
+    current_price: coin.current_price,
+  }));
+
+  return coinsData;
 };
 
 export default fetchCoins;
