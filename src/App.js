6229cd4e12db2d6e2efebe2fcdf9b126510ba7c1
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-26 06:47:25 +0300

    Fixed errors that were being generated by App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import DetailPage from "./pages/DetailPage";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes
+import HomePage from './pages/HomePage';
+import CoinDetailPage from './pages/CoinDetailPage';
+import Navigation from './components/Navigation';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/category/:categoryId" element={<DetailPage />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/details/:coinId" element={<CoinDetailPage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
