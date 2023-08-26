f00f9c13db031455c6020966cd0bc1d2fc7b082e
Peter-Kibet <peter.kibet@hotmail.com>
2023-08-26 06:45:20 +0300

    created the fetchCoin.js to fetch coins and a global daata element

---
+++ src/services/globalData.js
@@ -0,0 +1,14 @@
+// services/globalData.js
+import axios from 'axios';
+
+const API_URL = 'https://api.example.com'; // Replace with your API URL
+
+export const fetchGlobalData = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/global`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching global data:', error);
+    throw error;
+  }
+};
